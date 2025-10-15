814f5a071126a150681c6237413f7d2fbb1113d2
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-10-15 16:23:55 +0000

    update

--- Day-04/aws-first-instance/instance.tf
+++ Day-04/aws-first-instance/instance.tf
@@ -4,20 +4,14 @@ resource "aws_key_pair" "key-tf" {
   public_key = var.public_key
 }
 
-
 # public_key = file("${path.module}/id_rsa.pud")
 
 
-output "keyname" {
-  value = aws_key_pair.key-tf.key_name
+resource "aws_instance" "web" {
+  ami           = "ami-0933f1385008d33c4"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.key-tf.key_name
+  tags = {
+   Name = "my-first-tf-instance"
+ }
 }
-
-
-
-#resource "aws_instance" "web" {
-#  ami           = "ami-0933f1385008d33c4"
-#  instance_type = "t2.micro"
-#  tags = {
-#   Name = "my-first-tf-instance"
-# }
-#}
