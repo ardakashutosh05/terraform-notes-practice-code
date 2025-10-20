a26b2d6e6c9bc7666c3a083f87dfcaff7c63c51c
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-10-20 17:17:43 +0000

    update

--- Day-04/aws-first-instance/instance.tf
+++ Day-04/aws-first-instance/instance.tf
@@ -1,8 +1,10 @@
 
+
 # creating ssh-key
 resource "aws_key_pair" "key-tf" {
   key_name   = "key-tf"
-  public_key = var.public_key
+   public_key = var.public_key
+
 }
 
 #< public_key = file("${path.module}/id_rsa.pud")
@@ -27,9 +29,9 @@ resource "aws_security_group" "allow_tls" {
 
 # creating instance
 resource "aws_instance" "web" {
-  ami             = "ami-0933f1385008d33c4"
-  instance_type   = "t2.micro"
-  key_name        = aws_key_pair.key-tf.key_name
+  ami                    = "ami-0933f1385008d33c4"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key-tf.key_name
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
