057aa43d2266bb3f3bdf36bb52da9c9c679608fb
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-11-04 17:07:51 +0000

    add file

--- Day-04/aws-first-instance/instance.tf
+++ Day-04/aws-first-instance/instance.tf
@@ -1,8 +1,29 @@
 
+# datasource in terraform 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["${var.image_name}"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 # creating instance
 resource "aws_instance" "web" {
-  ami                    = var.image_id
+  #ami                    = var.image_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.key-tf.key_name
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
@@ -31,7 +52,7 @@ resource "aws_instance" "web" {
     #on_failure = continue
     #when = destroy
     command = "env>env.txt"
-    enviroment = {
+    environment = {
       envname = "envvalue"
     }
   }
