64c8f4b38aec6b5b5c5e52528b8368e9b919eeed
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-11-04 10:47:27 +0000

    update

---
+++ Day-04/aws-datasource/provider.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+# datasource in terraform 
+data "aws_ami" "ubuntu" {
+  most_recent = 
+  owners = ["099720109477"]
+
+  # name = amazon/ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20251015
+  # root device type = ebs
+  # vert type = hvm
+
+  filter {
+    name = "name"
+    values = ["amazon/ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
