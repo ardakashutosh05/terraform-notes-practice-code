4dbe1d3cabea658e0e76016825eec49569538013
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-11-06 09:54:02 +0000

    update

--- Day-06/aws-first-instance/instance.tf
+++ Day-06/aws-first-instance/instance.tf
@@ -25,8 +25,8 @@ resource "aws_instance" "web" {
   #ami                    = var.image_id
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.key-tf1.key_name
-  vpc_security_group_ids = [aws_security_group.allow_tls1.id]
+  key_name               = aws_key_pair.key-tf.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "my-first-tf-instance"
