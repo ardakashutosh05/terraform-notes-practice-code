2a40f39ca584d3c00d6667aba1009fb91bfb1431
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-11-06 18:11:37 +0000

    update

--- Day-07/modules/webserver/resource.tf
+++ Day-07/modules/webserver/resource.tf
@@ -1,8 +1,18 @@
-resource "aws_instance" "web" {
+ 
+ # instance
+ resource "aws_instance" "web" {
   ami           = var.image_id
   instance_type = var.instance_type
 
   tags = {
-    Name = "MyWebServer"
+    Name = "ModuleDirectRun"
   }
+
+
+  #key_name               = aws_key_pair.key-tf.key_name
 }
+
+resource "aws_key_pair" "key-tf" {
+    key_name   = "key-tf"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
