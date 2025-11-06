1b67624f93a91faa7105524545bf531370267aab
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-11-06 17:34:58 +0000

    up

--- Day-07/instance.tf
+++ Day-07/instance.tf
@@ -1,1 +1,9 @@
 # creating instance
+
+
+ module "mywebserver" {
+   source = "./modules/webserver"
+
+   instance_type = var.instance_type
+   image_id = var.image_id
+ }
--- Day-07/terraform.tf
+++ Day-07/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = "1.13.3"
-  
+
   # terraform version comand
 
   required_providers {
